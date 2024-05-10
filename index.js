e1fcaa572354c082a20120ef08ba895d98fab94f
tuii666 <evieblowerpun0suy@hotmail.com>
2024-05-10 09:08:12 +0700

    aptos sdk project

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function reverseList(head) {
-  let prev = null;
-  let curr = head;
-  while (curr) {
-    const next = curr.next;
-    curr.next = prev;
-    prev = curr;
-    curr = next;
-  }
-  return prev;
+function getMax(arr) {
+  return Math.max(...arr);
 }

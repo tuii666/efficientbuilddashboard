d7da39ad033e68348fa13a46087a0d441913796d
tuii666 <evieblowerpun0suy@hotmail.com>
2024-05-10 09:14:19 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-function getMax(arr) {
-  return Math.max(...arr);
+function addTwoNumbers(l1, l2) {
+  const dummy = new ListNode(0);
+  let p = l1,
+    q = l2,
+    curr = dummy;
+  let carry = 0;
+  while (p !== null || q !== null) {
+    const x = p !== null ? p.val : 0;
+    const y = q !== null ? q.val : 0;
+    const sum = x + y + carry;
+    carry = Math.floor(sum / 10);
+    curr.next = new ListNode(sum % 10);
+    curr = curr.next;
+    if (p !== null) p = p.next;
+    if (q !== null) q = q.next;
+  }
+  if (carry > 0) {
+    curr.next = new ListNode(carry);
+  }
+  return dummy.next;
 }

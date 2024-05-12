617f42b0e110445694122b7054605ed652684d42
tuii666 <evieblowerpun0suy@hotmail.com>
2024-05-12 14:56:26 +0700

    init project

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-function reverseString(str) {
-  return str.split("").reverse().join("");
+function findCircleNum(M) {
+  if (!M || M.length === 0) return 0;
+  const visited = new Array(M.length).fill(false);
+  let count = 0;
+  for (let i = 0; i < M.length; i++) {
+    if (!visited[i]) {
+      dfs(M, i, visited);
+      count++;
+    }
+  }
+  return count;
+}
+function dfs(M, i, visited) {
+  visited[i] = true;
+  for (let j = 0; j < M.length; j++) {
+    if (M[i][j] === 1 && !visited[j]) {
+      dfs(M, j, visited);
+    }
+  }
 }

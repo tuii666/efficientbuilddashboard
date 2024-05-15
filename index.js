ea012f02ce9502ddd7e057f4bace4f318fc7df42
tuii666 <evieblowerpun0suy@hotmail.com>
2024-05-15 13:59:24 +0700

    test project commit

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-const randomNumber = Math.floor(Math.random() * 100) + 1;
-console.log(randomNumber);
+function combine(n, k) {
+  const result = [];
+  backtrack(1, []);
+  return result;
+  function backtrack(start, current) {
+    if (current.length === k) {
+      result.push([...current]);
+      return;
+    }
+    for (let i = start; i <= n; i++) {
+      current.push(i);
+      backtrack(i + 1, current);
+      current.pop();
+    }
+  }
+}
